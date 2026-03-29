026682dca297986ed7cc126dc5cfe944f695416d
Vaishnavi-elluri <ellurivaishnavi55@gmail.com>
2026-03-29 21:47:55 +0530

    Change API key configuration method
    Replaced API key configuration with a hardcoded key.

--- app (2).py
+++ app (2).py
@@ -3,8 +3,7 @@ import streamlit as st
 import google.generativeai as genai
 import os
 import time
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
+genai.configure(api_key="AIzaSyDxq34Z3EtWUEA8uSfgOAigJgwOAQQ0yCc")
 st.title("🚀An AI Code Reviewer🚀")
 
 user_prompt= st.text_area("Enter your Python code here..")
