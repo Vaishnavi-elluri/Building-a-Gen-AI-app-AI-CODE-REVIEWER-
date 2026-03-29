ae409c3092cd460fb5a35bbd47355869474dea2a
Vaishnavi-elluri <ellurivaishnavi55@gmail.com>
2026-03-29 21:44:00 +0530

    Measure response time for AI content generation
    Added response time measurement for content generation.

--- app (2).py
+++ app (2).py
@@ -2,6 +2,7 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+import time
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 st.title("🚀An AI Code Reviewer🚀")
@@ -21,10 +22,15 @@ If the code is not Python, politely say you are a Python code reviewer."""
     )
 
     if user_prompt:
-        with st.spinner("Generating response..."):
-            try:
-                response = model.generate_content(user_prompt)
-                st.write(response.text)
-            except Exception as e:
-                st.error(f"Error: {e}")
+          with st.spinner("Generating response..."):
+                try:
+                    start = time.time()
+                    response = model.generate_content(user_prompt)
+                    end = time.time()
+            
+                    st.write(f"Response time: {round(end - start, 2)} seconds")
+                    st.write(response.text)
+            
+                except Exception as e:
+                    st.error(f"Error: {e}")
             
